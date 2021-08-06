3c0b407d06216e59c6dba48ee0e7bb248a363610
Adam Pugh <pughadam@hotmail.com>
2021-08-06 10:06:11 -0700

    Reading in Data

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,5 @@
-// @TODO: YOUR CODE HERE!
+// Load data.csv from data file - name as healthData
+d3.csv("data.csv").then(function(healthData) {
+
+    // Print the healthData
+    console.log(healthData);
